36ccf040e436ea5c6d375db4d42159f831b984aa
Unai Motriko <unaimotriko@gmail.com>
2017-07-09 19:14:11 +0100

    adds test folder and first unit test

---
+++ UnitTests/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+namespace UnitTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void Test_if_class_is_instantiable()
+        {
+            var credit = new Credit();
+            Assert.IsInstanceOf<Credit>(credit);
+        }
+    }
+}

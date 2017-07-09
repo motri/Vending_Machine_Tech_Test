5e66636b4d35e43cfe920b40ac7c762e3a3bd990
Unai Motriko <unaimotriko@gmail.com>
2017-07-09 19:20:57 +0100

    Adds first passing test

--- UnitTests/Test.cs
+++ UnitTests/Test.cs
@@ -1,15 +1,23 @@
 ﻿using NUnit.Framework;
 using System;
+using VendingMachineTechTest;
 namespace UnitTests
 {
     [TestFixture()]
     public class Test
     {
+        Credit credit;
+        [SetUp]
+        public void SetUp()
+        {
+            credit = new Credit();
+        }
+
         [Test()]
         public void Test_if_class_is_instantiable()
         {
-            var credit = new Credit();
             Assert.IsInstanceOf<Credit>(credit);
         }
+
     }
 }

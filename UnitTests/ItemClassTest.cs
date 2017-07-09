014e3c44ab4392f2a87cad00aca9ad7eda2b9d0c
Unai Motriko <unaimotriko@gmail.com>
2017-07-09 23:29:42 +0100

    Test Item class contains list Hashtable

---
+++ UnitTests/ItemClassTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using VendingMachineTechTest;
+namespace UnitTests
+{
+    [TestFixture()]
+    public class ItemClassTest
+    {
+        Item item;
+
+        [SetUp]
+        public void SetUp()
+        {
+            item = new Item();
+        }
+
+        [Test()]
+        public void Class_can_be_initialized()
+        {
+            Assert.IsInstanceOf<Item>(item);
+        }
+
+        [Test()]
+        public void Class_contains_a_hash_when_initialized()
+        {
+            Assert.IsInstanceOf<Hashtable>(item.list);
+        }
+    }
+}
---
+++ VendingMachineTechTest/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+namespace VendingMachineTechTest
+{
+    public class Item
+    {
+        public Hashtable list;
+
+        public Item()
+        {
+            list = new Hashtable();
+            list.Add("Water", 0.60);
+            list.Add("Crips", 0.40);
+        }
+    }
+}

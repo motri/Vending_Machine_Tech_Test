edf2a450f7737042f650213c42e03b675a8d76cc
Unai Motriko <unaimotriko@gmail.com>
2017-07-10 02:30:42 +0100

    Adds AddItem method and test

--- UnitTests/BasketClassTest.cs
+++ UnitTests/BasketClassTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections;
 using VendingMachineTechTest;
 namespace UnitTests
 {
@@ -12,10 +13,30 @@ namespace UnitTests
         {
             basket = new Basket();
         }
+
         [Test()]
-        public void TestCase()
+        public void Class_can_instantiated()
         {
             Assert.IsInstanceOf<Basket>(basket);
+        }
+
+        [Test()]
+		public void Class_contains_a_hash_when_initialized()
+		{
+			Assert.IsInstanceOf<Hashtable>(basket.items);
+		}
+        [Test()]
+        public void Hash_is_empty_when_initialized()
+        {
+            Assert.IsEmpty(basket.items);
+        }
+
+        [Test()]
+        public void Item_is_added_to_basket_by_AddItem_method()
+        {
+            basket.AddItem("Water", 1);
+            Assert.IsNotEmpty(basket.items);
+            Assert.AreEqual(basket.items["Water"], 1);
         }
     }
 }
--- VendingMachineTechTest/Basket.cs
+++ VendingMachineTechTest/Basket.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections;
 namespace VendingMachineTechTest
 {
     public class Basket
     {
+        public Hashtable items;
         public Basket()
         {
+            items = new Hashtable();
+        }
+
+        public void AddItem(string itemName, int itemQuantity)
+        {
+            items.Add(itemName, itemQuantity);
         }
     }
 }

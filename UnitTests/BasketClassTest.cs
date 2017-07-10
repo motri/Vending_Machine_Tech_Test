b88bbe9fdfb854c7aa84cd2de5a9b514bb51057a
Unai Motriko <unaimotriko@gmail.com>
2017-07-10 03:05:18 +0100

    Add constrains to AddItem method

--- UnitTests/BasketClassTest.cs
+++ UnitTests/BasketClassTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.IO;
 using System.Collections;
 using VendingMachineTechTest;
 namespace UnitTests
@@ -38,5 +39,25 @@ namespace UnitTests
             Assert.IsNotEmpty(basket.items);
             Assert.AreEqual(basket.items["Water"], 1);
         }
+
+        [Test()]
+        public void Only_items_available_to_add_are_water_and_crisps()
+        {
+            
+			using (StringWriter alert = new StringWriter())
+			{
+				Console.SetOut(alert);
+				basket.AddItem("Soda", 4);
+				Assert.AreEqual("Item not avaliable.\n", alert.ToString());
+			}
+        }
+
+		[Test()]
+		public void If_product_already_in_basket_quantity_added_to_previous_number()
+		{
+			basket.AddItem("Water", 1);
+            basket.AddItem("Water", 3);
+            Assert.AreEqual(basket.items["Water"], 4);
+		}
     }
 }
--- VendingMachineTechTest/Basket.cs
+++ VendingMachineTechTest/Basket.cs
@@ -5,14 +5,34 @@ namespace VendingMachineTechTest
     public class Basket
     {
         public Hashtable items;
+        public Item item;
         public Basket()
         {
             items = new Hashtable();
+            item = new Item();
         }
 
         public void AddItem(string itemName, int itemQuantity)
         {
-            items.Add(itemName, itemQuantity);
+            if (item.list.ContainsKey(itemName))
+            {
+                if (items.ContainsKey(itemName))
+                {
+                    int number;
+                    number = (int)items[itemName];
+                    items[itemName] = number + itemQuantity;
+                }
+                else
+                {
+                    items.Add(itemName, itemQuantity); 
+                }
+
+            } 
+            else
+            {
+                Console.WriteLine("Item not avaliable.");
+            }
+
         }
     }
 }

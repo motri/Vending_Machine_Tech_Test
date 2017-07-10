05eb06afb63cad222b44fec24f38cee89cc21d94
Unai Motriko <unaimotriko@gmail.com>
2017-07-10 18:41:50 +0100

    Adds Printer class

--- UnitTests/PrinterClassTest.cs
+++ UnitTests/PrinterClassTest.cs
@@ -1,9 +1,17 @@
 ﻿using NUnit.Framework;
+using VendingMachineTechTest;
 namespace UnitTests
 {
     [TestFixture]
     public class PrinterClassTest
     {
+        Printer printer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            printer = new Printer();
+        }
         [Test]
         public void TestCase()
         {
---
+++ VendingMachineTechTest/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace VendingMachineTechTest
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}

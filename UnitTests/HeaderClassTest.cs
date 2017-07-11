2af3a0d7f5a14fbf96ebffbaa2f31892c2a81cff
Unai Motriko <unaimotriko@gmail.com>
2017-07-11 21:50:20 +0100

    Adds credit and product info to header class

--- UnitTests/HeaderClassTest.cs
+++ UnitTests/HeaderClassTest.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
+using System.IO;
+using System.Collections;
 using VendingMachineTechTest;
 namespace UnitTests
 {
@@ -8,15 +10,33 @@ namespace UnitTests
     public class HeaderClassTest
     {
 		Header header;
+        Hashtable productList;
+        Decimal credit;
+
 		[SetUp]
 		public void SetUp()
 		{
 			header = new Header();
+			productList = new Hashtable
+			{
+				{ "Water", 0.6m },
+				{ "Crisps", 0.4m }
+			};
+            credit = 4.0m;
 		}
 
         [Test]
         public void TestCase()
-        {
+		{
+			using (StringWriter headerOutput = new StringWriter())
+
+			{
+                Console.SetOut(headerOutput);
+                header.Print(productList, credit);
+                Assert.AreEqual("PRODUCTS: \nCrisps: 0,40 €\nWater: 0,60 €\nCREDIT: 4,00 €\n", headerOutput.ToString());
+
+			}
+       
         }
     }
 }
--- VendingMachineTechTest/Header.cs
+++ VendingMachineTechTest/Header.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections;
 namespace VendingMachineTechTest
 {
     public class Header
     {
         public Header()
         {
+        }
+
+        public void Print(Hashtable productList, decimal credit)
+        {
+            Console.WriteLine("PRODUCTS: ");
+
+            foreach (DictionaryEntry pair in productList)
+            {
+                Console.WriteLine("{0}: {1:C}", pair.Key, pair.Value);
+            }
+
+            Console.WriteLine("CREDIT: {0:C}", credit);
         }
     }
 }

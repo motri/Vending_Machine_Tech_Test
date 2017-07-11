497d498c0040445850d59604701e97539cd0593e
Unai Motriko <unaimotriko@gmail.com>
2017-07-11 10:50:03 +0100

    Adds header class

---
+++ UnitTests/HeaderClassTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using VendingMachineTechTest;
+namespace UnitTests
+{
+
+    [TestFixture]
+    public class HeaderClassTest
+    {
+		Header header;
+		[SetUp]
+		public void SetUp()
+		{
+			header = new Header();
+		}
+
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
